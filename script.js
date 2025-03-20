1dd6e7ffcae186b7a7ac08f80107c9ab954c8017
George Papoulidis <geopapougr@gmail.com>
2025-03-20 12:44:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('push-btn').addEventListener('click', () => {
+    Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+            console.log('Push notifications granted!');
+            // You can now send a push notification via Gozen Notify
+            subscribeUserToPush();
+        } else {
+            console.log('Push notifications denied!');
+        }
+    });
+});
+
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+    });
+}
+
